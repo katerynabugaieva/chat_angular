ad9f3d15c1aeff4e5dee2dc97091db6fb688df68
Kateryna Bugaieva <sonnensmile@gmail.com>
2019-12-12 12:13:31 +0100

    update html

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,19 +1,46 @@
-import { Component } from '@angular/core';
-import { ChatService } from './chat.service';
+import {Component, OnInit} from '@angular/core';
+import {ChatService} from './chat.service';
+
+import * as moment from 'moment';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/skipWhile';
+import 'rxjs/add/operator/scan';
+import 'rxjs/add/operator/throttleTime';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular chat';
+export class AppComponent implements OnInit {
   message: string;
-  constructor(private chatService: ChatService) { }
+  messages: string[] = [];
+  secretCode: string;
+
+  constructor(private chatService: ChatService) {
+    this.secretCode = 'DONT TELL';
+  }
 
   sendMessage() {
     this.chatService.sendMessage(this.message);
     this.message = '';
   }
 
+  ngOnInit() {
+    this.chatService
+      .getMessages()
+      .distinctUntilChanged()
+      .filter((message) => message.trim().length > 0)
+      .throttleTime(1000)
+      .skipWhile((message) => message !== this.secretCode)
+      .scan((acc: string, message: string, index: number) =>
+          `${message}(${index + 1})`
+        , 1)
+      .subscribe((message: string) => {
+        const currentTime = moment().format('hh:mm:ss a');
+        const messageWithTimestamp = `${currentTime}: ${message}`;
+        this.messages.push(messageWithTimestamp);
+      });
+  }
 }
